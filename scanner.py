f3411318c7dca582cd2a6d0f1ec24aa68a57e88b
Jay <jayghughri1@gmail.com>
2024-05-31 17:01:54 +0530

    commit2

--- scanner.py
+++ scanner.py
@@ -14,3 +14,11 @@ if len(sys.argv) == 2:
 else:
     print("invalid amount of arguments")
     print("Syntax: python3 scanner.py  <ip>")
+
+
+# add a nice banner
+
+print("$" * 50)
+print("Scanning target : " + target)
+print("Time Started : " + str(datetime.now()))
+print("$" * 50)
