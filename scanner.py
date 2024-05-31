0c5bffb9e9ef85b7ee9c9b481877bf4490f53d25
Jay <jayghughri1@gmail.com>
2024-05-31 17:46:52 +0530

    final _commit

--- scanner.py
+++ scanner.py
@@ -19,6 +19,31 @@ else:
 # add a nice banner
 
 print("$" * 50)
+print("\n\n")
 print("Scanning target : " + target)
 print("Time Started : " + str(datetime.now()))
 print("$" * 50)
+print("\n\n")
+
+try:
+    for port in range(50, 85):  # you can set your range according to your preferences
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, port))
+
+        if result == 0:
+            print(f"Port {port} is open")
+        s.close()
+
+
+except KeyBoardInterrupt:
+    print("\nExiting Program")
+    sys.exit()
+
+except socket.gaierror:
+    print("Hostname could not be resolved")
+    sys.exit()
+
+except socket.error:
+    print("could not connect to server")
+    sys.exit()
